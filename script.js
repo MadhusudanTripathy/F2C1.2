aec6da4b9b6f7a8ef99f202a4772f9ad73ae84b1
Madhusudan Tripathy <57895240+MadhusudanTripathy@users.noreply.github.com>
2023-05-13 20:51:21 +0530

    P2R

--- script.js
+++ script.js
@@ -8,20 +8,39 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map((e)=>{
+    console.log(e);
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((element)=>{
+        console.log(element);
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({id:4,name:"susan",age:"20",marks:45});
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr= arr.filter((e)=>{
+    return e.marks>50
+  })
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let mrr = [
+    { id: 5, name: "MST", age: "19", marks: 62 },
+    { id: 6, name: "GST", age: "21", marks: 74 },
+    { id: 7, name: "LST", age: "18", marks: 37 },
+  ];
+  arr=arr.concat(mrr);
+  console.log(arr);
 }
